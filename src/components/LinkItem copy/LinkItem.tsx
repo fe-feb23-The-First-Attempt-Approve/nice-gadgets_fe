72371e33b2c2a47d829821e6ca072fc6ff2f46fb
Myla Voronevska <code.crystal.008@gmail.com>
2023-06-09 13:41:08 +0300

    relocated & divided files

--- src/components/LinkItem copy/LinkItem.tsx
+++ src/components/LinkItem copy/LinkItem.tsx
@@ -2,18 +2,18 @@ import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 import cn from 'classnames';
 
-interface Props {
+interface LinkItemProps {
   to: string;
   label: string;
 }
 
-export const LinkItem: FC<Props> = ({ to, label }) => {
+export const LinkItem: FC<LinkItemProps> = ({ to, label }) => {
   return (
     <li className="nav__item">
       <NavLink
         to={to}
-        className={({ isActive }) => cn('nav__link',
-          { 'nav__link--active': isActive })}
+        className={(active) => cn('nav__link',
+          { 'nav__link--active': active })}
       >
         {label}
       </NavLink>
