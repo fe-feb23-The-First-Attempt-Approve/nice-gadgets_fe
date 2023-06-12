261dee1357d08db147e3261a00e0b69c6ab00512
Myla Voronevska <code.crystal.008@gmail.com>
2023-06-13 00:19:10 +0300

    Cart

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,7 +1,17 @@
 export const CartPage = () => {
   return (
-    <div className="container">
-      <h1 className="title">💵 Cart page</h1>
+    <div className="container gadgets-page">
+      <h1 className="gadgets-page__title">Cart</h1>
+
+      <div className="cart">
+        <div className="cart__card">
+          cart items
+        </div>
+
+        <div className="cart__billing">
+          billing
+        </div>
+      </div>
     </div>
   );
 };
