6f9b5e518053d4fe59cb08e12739eb2cbe89181f
Paul <114941446+PashaMalyshkin@users.noreply.github.com>
2023-06-14 20:39:54 +0300

    Add notification (#35)
    * added responsive hot-price-slider
    
    * add-notification
    
    * deleted word phone in message

--- src/components/HeartButton/HeartButton.tsx
+++ src/components/HeartButton/HeartButton.tsx
@@ -1,11 +1,13 @@
 import {
   FC, useContext, useEffect, useMemo,
 } from 'react';
+import { toast } from 'react-toastify';
 import { IconLikeEmpty } from '../Icons/IconLikeEmpty';
 import { IconLikeFull } from '../Icons/IconLikeFull';
 import { CountFavoritesContext } from '../../providers/CountFavorites';
 import { useLocalStorage } from '../../customHooks/useLocalStorage';
 import { Gadget } from '../../types/Gadget';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface Props {
   gadget: Gadget;
@@ -19,6 +21,20 @@ export const HeartButton: FC<Props> = ({ gadget }) => {
       id === gadget.id))),
   [favoriteIds, gadget.id]);
 
+  const notifyFavorite = () => toast.success(
+    `❤️ ${gadget.name} has been added to favorites`,
+    {
+      position: 'bottom-left',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'light',
+    },
+  );
+
   useEffect(() => {
     updateCountFavorites(favoriteIds.length);
   }, [favoriteIds, gadget.id]);
@@ -30,6 +46,10 @@ export const HeartButton: FC<Props> = ({ gadget }) => {
     setFavoriteIds((isLiked)
       ? favorites.filter(({ id }: Gadget) => gadget.id !== id)
       : [...favorites, gadget]);
+
+    if (!isLiked) {
+      notifyFavorite();
+    }
   };
 
   return (
