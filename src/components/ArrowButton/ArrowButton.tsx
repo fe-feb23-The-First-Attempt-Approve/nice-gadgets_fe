06df6e2ee1a7711868b8d25d0528d55968d7e66e
Mila Voronevska <125828769+voronevska@users.noreply.github.com>
2023-06-14 10:26:19 +0300

    Cart tab (#31)
    * Cart
    
    * Main page fixes
    
    * Cart layout

--- src/components/ArrowButton/ArrowButton.tsx
+++ src/components/ArrowButton/ArrowButton.tsx
@@ -1,15 +1,22 @@
+import { FC } from 'react';
 import { IconArrow } from '../Icons/IconArrow';
 
 interface Props {
   arrowDirection: 'top' | 'down' | 'right' | 'left';
+  // eslint-disable-next-line react/require-default-props
+  hasBackground?: boolean;
 }
 
-export const ArrowButton: React.FC<Props> = ({ arrowDirection }) => {
+export const ArrowButton: FC<Props> = ({ arrowDirection, hasBackground }) => {
   const arrowContainerClassName = `arrow-container arrow-container--${arrowDirection}`;
 
+  const buttonClassName = hasBackground
+    ? `${arrowContainerClassName} arrow-container-fill`
+    : arrowContainerClassName;
+
   return (
-    <div className={arrowContainerClassName}>
+    <button type="button" className={buttonClassName}>
       <IconArrow />
-    </div>
+    </button>
   );
 };
