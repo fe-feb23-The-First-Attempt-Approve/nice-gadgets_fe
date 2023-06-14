b10a04b626de40359376d6753c5da1f5c8910474
Mykyta Holtsov <123888109+M1k1ta@users.noreply.github.com>
2023-06-14 13:04:12 +0300

    fix img alt in card and add tablet photos (#32)

--- src/pages/AccessoriesPage/AccessoriesPage.tsx
+++ src/pages/AccessoriesPage/AccessoriesPage.tsx
@@ -7,7 +7,11 @@ export const AccessoriesPage = () => {
     <div className="container">
       <Breadcrumbs category={category} />
 
-      <h1 className="title">Accessories page</h1>
+      <h1 className="gadgets-page__title">Accessories page</h1>
+
+      <p className="gadgets-page__description">
+        {`${0} models`}
+      </p>
     </div>
   );
 };
