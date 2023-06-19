03a32dfbe1ecacbf71cd81b532ad1a49d94d8898
Pasha <pashamalyshkin2003@gmail.com>
2023-06-19 10:55:08 +0300

    added api for auth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,5 @@
-import { client } from "../utils/fetchClient"
+import { client } from '../utils/fetchClient';
 
 export const register = (userName: string, email: string, password: string) => {
   return client.post('/registration', { userName, email, password });
-}
+};
