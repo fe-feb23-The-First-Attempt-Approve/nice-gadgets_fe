6654778bb5d6aa0d6d3ef778a515bde9de0975ab
Paul <114941446+PashaMalyshkin@users.noreply.github.com>
2023-06-19 16:20:32 +0300

    added products context / fixed categories section / fixed linter mistakes (#61)

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,6 +1,14 @@
+import { FC } from 'react';
 import { CategoryCard } from '../CategoryCard';
 
-export const Categories = () => {
+interface Props {
+  productsAmount: {
+    phonesAmount: number,
+    tabletsAmount: number,
+    accessoriesAmount: number
+  }
+}
+export const Categories: FC<Props> = ({ productsAmount }) => {
   return (
     <div className="categories">
       <h2 className="categories__title">Shop by category</h2>
@@ -11,21 +19,21 @@ export const Categories = () => {
           // eslint-disable-next-line
           image="https://static.independent.co.uk/2021/10/04/14/iphones%20IndyBest.jpg"
           title="Modile phones"
-          amount={1}
+          amount={productsAmount.phonesAmount}
         />
         <CategoryCard
           link="/tablets"
           // eslint-disable-next-line
           image="https://hips.hearstapps.com/hmg-prod/images/apple-ipad-8th-generation-1600702336.jpg"
           title="Tablets"
-          amount={2}
+          amount={productsAmount.tabletsAmount}
         />
         <CategoryCard
           link="/accessories"
           // eslint-disable-next-line
           image="https://m.economictimes.com/thumb/msid-98599217,width-3000,height-3000,resizemode-4,imgsize-288842/apple1-.jpg"
           title="Accessories"
-          amount={3}
+          amount={productsAmount.accessoriesAmount}
         />
       </div>
     </div>
