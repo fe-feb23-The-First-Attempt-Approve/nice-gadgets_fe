f35d9a4763d246968521f344f0b92427343ef574
Pasha <pashamalyshkin2003@gmail.com>
2023-06-20 10:42:44 +0300

    auth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,5 +2,8 @@ import { client } from '../utils/fetchClient';
 import { RegistrationResponse } from '../types/Response';
 
 export const register = (userName: string, email: string, password: string) => {
-  return client.post<RegistrationResponse>('/registration', { userName, email, password });
+  return client.post<RegistrationResponse>(
+    '/registration',
+    { userName, email, password },
+  );
 };
