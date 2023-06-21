111dee91f8924407cbc5f6428b64687d6ce016c1
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-06-21 19:02:04 +0300

    fixes

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import { NotFoundBlack } from '../../components/NotFoundBlack';
 import { NotFoundWhite } from '../../components/NotFoundWhite';
 import { useTheme } from '../../providers/ThemeContext';
 
 export const NotFoundPage = () => {
   const { theme } = useTheme();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, []);
 
   return (
     <div className="not-found">
